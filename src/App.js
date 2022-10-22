658a0c0cef2652f56f46f15c9a4df2dbe2cf5bb9
aesweb <aes.sncr@gmail.com>
2022-10-22 11:34:58 +0300

    components created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import Header from 'components/Header';
+import HeroSection from 'components/HeroSection';
+import Categories from 'components/Categories';
+import Campaings from 'components/Campaigns';
+import Favorites from 'components/Favorites';
+import MobileApp from 'components/MobileApp';
+import Cards from 'components/Cards';
+import Footer from 'components/Footer';
+
 function App() {
-  return <div className="App">test</div>;
+  return (
+    <>
+      <Header />
+      <HeroSection />
+      <Categories />
+      <Campaings />
+      <Favorites />
+      <MobileApp />
+      <Cards />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
