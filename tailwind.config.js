b2c432d1bae9b2b30b4ea9e6add2afd23710cd8e
aesweb <aes.sncr@gmail.com>
2022-10-22 11:28:08 +0300

    tailwind config prepared

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: 'jit',
-  content: ['./src/**/*.{html,js}'],
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'brand-color': '#4c3398',
+        'primary-brand-color': '#5d3ebc',
+        'secondary-brand-color': '#7849f7',
+        'brand-yellow': '#ffd300',
+      },
+    },
   },
   plugins: [],
 };
